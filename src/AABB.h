44528778514bd840caf2d54e05a54511ed272562
Prabesh Paudel <poudelprabesh2277@gmail.com>
2024-10-17 12:54:52 +0545

    i was wrong

--- src/AABB.h
+++ src/AABB.h
@@ -5,12 +5,5 @@
      AABB(Vector2 ,Vector2 );
      AABB(float ,float ,float ,float );
      AABB(); 
-     
- AABB::AABB(Vector2 Max,Vector2 Min): max(Max),min(Min)
- {}
-AABB::AABB(float minx,float maxx,float miny,float maxy): max(Vector2{maxx,maxy}),min(Vector2{minx,miny})
-{}
-AABB::AABB():max(Vector2{0,0}),min(Vector2{0,0})
-{}
 };
 
--- src/Polygon.h
+++ src/Polygon.h
@@ -9,24 +9,4 @@
 void DrawFilledPolygon( std::vector<Vector2>& points, Color color);
 void DrawPolygonLine( std::vector<Vector2>& points, Color color);
 
-void DrawFilledPolygon( std::vector<Vector2>& points, Color color)
-{
-    if (points.size() < 3) return;
-
-    for (int i = 1; i < (int)points.size() - 1; i++)
-    {
-        DrawTriangle(points[0], points[i], points[i + 1], color);
-    }
-}
-
-void DrawPolygonLine( std::vector<Vector2>& points, Color color)
-{
-    if (points.size() < 3) return;
-
-    for (int i = 0; i < (int)points.size(); i++)
-    {
-        DrawLineV(points[i], points[(i + 1) % points.size()], color);
-    }
-}
-
 #endif 
