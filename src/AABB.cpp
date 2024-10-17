57deb8fb68c3a689a46ee6d7021fcb99b42331e0
Prabesh Paudel <poudelprabesh2277@gmail.com>
2024-10-17 12:50:44 +0545

    nothing important

--- src/AABB.cpp
+++
@@ -1,8 +0,0 @@
-#include "AABB.h"
- AABB::AABB(Vector2 Max,Vector2 Min): max(Max),min(Min)
- {}
-AABB::AABB(float minx,float maxx,float miny,float maxy): max(Vector2{maxx,maxy}),min(Vector2{minx,miny})
-{}
-AABB::AABB():max(Vector2{0,0}),min(Vector2{0,0})
-{}
-
